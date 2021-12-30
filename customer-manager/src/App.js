4e00dfad832013a65fae814c1f2c436740137a8b
UrielQuiroz <uriel.quiroz187@gmail.com>
2021-12-30 12:44:39 -0600

    React Router Dom
    Definiendo rutas con react-router-dom utilizando Route y Outle

--- customer-manager/src/App.js
+++ customer-manager/src/App.js
@@ -1,9 +1,22 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import IniciarSesion from './layout/IniciarSesion';
+import Layout from './layout/Layout';
+import Inicio from './paginas/Inicio';
+import LoginForm from './paginas/LoginForm';
 
 function App() {
   return (
-    <h1>Hola mundo</h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <IniciarSesion /> }>
+          <Route index element={ <LoginForm />} />
+        </Route>
+        <Route path='/clientes' element={ <Layout /> }>
+          <Route index element={ <Inicio /> } />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ customer-manager/src/paginas/Inicio.jsx
@@ -0,0 +1,11 @@
+//import {} from 'react-router-dom';
+
+const Inicio = () => {
+    return (
+        <div>
+            <h1>Desde el Inicio</h1>
+        </div>
+    )
+}
+
+export default Inicio
