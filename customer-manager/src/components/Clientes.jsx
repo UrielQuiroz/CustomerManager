f1eaf79a3e066dd80a7c9ca0cd4d25d9b960a378
UrielQuiroz <uriel.quiroz187@gmail.com>
2022-01-03 18:28:46 -0600

    GET
    Se utilizo fetch para obtener todos los clientes en la pagina Inicio y se creo el componente Cliente para mandarle todos los clientes que exiten en la base de datos

---
+++ customer-manager/src/components/Clientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Clientes = ({cliente}) => {
+    return (
+        <div>
+            <h1>{cliente.nombre}</h1>      
+        </div>
+    )
+}
+
+export default Clientes
--- customer-manager/src/paginas/Inicio.jsx
+++ customer-manager/src/paginas/Inicio.jsx
@@ -1,10 +1,52 @@
-//import {} from 'react-router-dom';
+import { useState, useEffect } from "react";
+import Clientes from "../components/Clientes";
 
 const Inicio = () => {
+
+    const [clientes, setClientes] = useState({})
+
+    useEffect(() => {
+
+        const obtenerClientesApi = async () => {
+            try {
+                const url = 'http://localhost:3004/clientes';
+                const rpta = await fetch(url);
+                const result = await rpta.json();
+                setClientes(result);
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        obtenerClientesApi();
+
+    }, [])
+
     return (
-        <div>
-            <h1>Desde el Inicio</h1>
-        </div>
+        <>
+            <h1 className='font-black text-4xl text-blue-700'>Clientes</h1>
+            <p className='mt-3'>Administra tus Clientes</p>
+
+            <table className="w-full mt-5 table-auto shadow bg-white">
+                <thead className="bg-blue-800 text-white">
+                    <tr>
+                        <th className="p-2">Nombre</th>
+                        <th className="p-2">Contacto</th>
+                        <th className="p-2">Empresa</th>
+                        <th className="p-2">Acciones</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    {clientes.map(cliente => (
+                        <Clientes 
+                            key={cliente.id}
+                            cliente={cliente}
+                        />
+                    ))}
+                </tbody>
+            </table>
+        </>
     )
 }
 
